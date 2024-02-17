574fe791e22afda7256c599f56cd7a427bcf421e
ahinestrog <124910059+ahinestrog@users.noreply.github.com>
2024-02-16 21:31:29 -0500

    Update automaton.py

--- automaton.py
+++ automaton.py
@@ -1,19 +1,19 @@
 import pyformlang.finite_automaton as automata
 
-# Creamos un autómata finito determinista, llamando la funció de la librería
+# Creating a Deterministic Automaton
 automat = automata.DeterministicFiniteAutomaton()
 
-# Creamos cuantos estados quereamos
+# Creating the amount the states 
 state0 = automata.State(0)
 state1 = automata.State(1)
 state2 = automata.State(2)
 
-# Agregar estados al DFA
+# Adding states to the DFA
 automat.add_start_state(state0)
 automat.add_final_state(state0)
 automat.add_final_state(state2)
 
-# Agregar transiciones
+# Adding transitions
 automat.add_transition(state0, 0, state0)
 automat.add_transition(state0, 1, state1)
 automat.add_transition(state1, 1, state0)
@@ -21,8 +21,8 @@ automat.add_transition(state1, 0, state2)
 automat.add_transition(state2, 1, state2)
 automat.add_transition(state2, 0, state1)
 
-# Verificar si el autómata acepta una cadena
+# Input for the user to enter the string
 cadena = input("->")
-cadena = [int(i) for i in cadena]  #Crea una lista haciendo que donde está parado se convierta en un entero
-print(automat.accepts(cadena)) # True or false
+cadena = [int(i) for i in cadena]  # Create a list by making where you stand become an integer
+print(automat.accepts(cadena)) # Returns True or false
 
